72057ec10ff16bd240f08a6cd12e638d8083d92d
Jae Yong Lee <leejaeyong7@gmail.com>
2015-09-11 16:37:01 -0500

    improved init.el for performance

--- init.el
+++ init.el
@@ -23,21 +23,34 @@
 
 
 
+(if window-system
+    (tool-bar-mode -1))
 
 ;;--------------------------------------------------------------------------------------------------
 ;;
 ;;    Package list setup
 ;;
 ;;--------------------------------------------------------------------------------------------------
-(tool-bar-mode -1)
 
-
+;lisp loadpath
+;(when (eq system-type 'gnu/linux) (add-to-list 'load-path "~/.emacs.d/lisp"))
 (require 'package) ;; You might already have this line
 (setq package-list '(multiple-cursors emmet-mode org ssh yasnippet flycheck evil writeroom-mode))
 (setq debug-on-error t)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+			               ("melpa" . "http://melpa.org/packages/")
+			 ))
+(package-initialize) ;; You might already have this line
+;(package-refresh-contents)
+
+;windows ssh activation
+(when (and (eq system-type 'windows-nt) (file-exists-p "C:\Program Files (x86)/PuTTY/plink.exe"))
+  (add-to-list 'package-list 'tramp))
 
 ;check system for package-list addition
 ;lisp interaction
+
 (when (or (when (eq system-type 'darwin)(file-exists-p "/usr/local/bin/sbcl"))(when (eq system-type 'gnu/linux) (file-exists-p "/bin/sbcl"))) (add-to-list 'package-list 'slime))
 
 ;haskell interaction
@@ -48,21 +61,6 @@
     (setq package-list (append package-list '(haskell-mode ghc cl-lib company company-ghc flycheck-haskell)))
     (when (eq system-type 'darwin) (add-to-list 'package-list 'exec-path-from-shell))))
 
-;lisp loadpath
-(when (eq system-type 'gnu/linux) (add-to-list 'load-path "~/.emacs.d/lisp"))
-;(when (file-exists-p "~/.emacs.d/misc") (add-to-list 'load-path "~/.emacs.d/misc"))
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-			               ("melpa" . "http://melpa.org/packages/")
-			 ;("melpa" . "http://melpa-stable.milkbox.net/packages/")
-			 ))
-(package-initialize) ;; You might already have this line
-;(package-refresh-contents)
-
-;windows ssh activation
-(when (and (eq system-type 'windows-nt) (file-exists-p "C:\Program Files (x86)/PuTTY/plink.exe"))
-  (add-to-list 'package-list 'tramp))
-
 
 
 
@@ -71,7 +69,7 @@
 ;;    Installation process
 ;;
 ;;--------------------------------------------------------------------------------------------------
-; fetch the list of packages available
+;fetch the list of packages available
 (unless package-archive-contents
   (package-refresh-contents))
 
@@ -131,29 +129,26 @@
                                        evil-yank)))
 ;;--------------------------------------------------------------------------------------------------
 
-(when (package-installed-p 'emmet-mode)
-  (add-to-list 'load-path "~/emacs.d/emmet-mode")
-  (require 'emmet-mode)
-  (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
-  (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
-  )
+
+(require 'emmet-mode)
+(add-to-list 'load-path "~/emacs.d/emmet-mode")
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 
 ;;--------------------------------------------------------------------------------------------------
-(when (package-installed-p 'slime)
-  (require 'slime)
-  ;; Set your lisp system and, optionally, some contribs
-  (when (eq system-type 'darwin)(setq inferior-lisp-program "/usr/local/bin/sbcl"))
-  (when (eq system-type 'gnu/linux) (setq inferior-lisp-program "/bin/sbcl"))
-  
-  (setq slime-contribs '(slime-fancy)))
+;; (when (package-installed-p 'slime)
+;;   (require 'slime)
+;;   ;; Set your lisp system and, optionally, some contribs
+;;   (when (eq system-type 'darwin)(setq inferior-lisp-program "/usr/local/bin/sbcl"))
+;;   (when (eq system-type 'gnu/linux) (setq inferior-lisp-program "/bin/sbcl"))
+;;  (setq slime-contribs '(slime-fancy)))
+
 ;;--------------------------------------------------------------------------------------------------
-(when (package-installed-p 'org)
-  (require 'org)
-  (global-set-key (kbd "C-c l") 'org-store-link)
-  (global-set-key (kbd "C-c c") 'org-capture)
-  (global-set-key (kbd "C-c a") 'org-agenda)
-  (global-set-key (kbd "C-c b") 'org-iswitchb)
-  )
+(require 'org)
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
 ;;--------------------------------------------------------------------------------------------------
 (when (package-installed-p 'haskell-mode)
   (if (eq system-type 'gnu/linux)(add-to-list 'exec-path "~/.cabal/bin"))
@@ -165,7 +160,7 @@
         (exec-path-from-shell-initialize)))
   ;(if (eq system-type 'windows-nt) (setq ghc-interactive-command "ghc-modi"))
   (require 'haskell-mode)
-  (load "haskell-mode-autoloads")
+  ;(load "haskell-mode-autoloads")
   (require 'haskell-interactive-mode)
   (require 'haskell-process)
   (require 'company)
@@ -230,7 +225,7 @@
 (add-hook 'js-mode-hook
           (lambda () (flycheck-mode t)))
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)
+(add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
 
 ;;--------------------------------------------------------------------------------------------------
 (recentf-mode 1)
@@ -249,10 +244,10 @@
 ;;--------------------------------------------------------------------------------------------------
 (add-hook 'asm-mode-hook
           (lambda () (interactive) (modify-syntax-entry ?# "< b")))
-
-;;--------------------------------------------------------------------------------------------------
-(require 'writeroom-mode)
     
+;; backup data-----------
+
+
 ;;--------------------------------------------------------------------------------------------------
 ;;
 ;;    Global ket setups
@@ -263,7 +258,7 @@
 (global-set-key (kbd "C-x C-b")(lambda () (interactive) (ibuffer t)))
 (global-unset-key [(shift f11)])
 
-(global-set-key [(shift f11)] 'writeroom-mode)
+(if (eq system-type 'darwin) (global-set-key (kbd "C-c C-f") 'writeroom-mode) (global-set-key [(shift f11)] 'writeroom-mode))
 
 
 ;;--------------------------------------------------------------------------------------------------
@@ -277,11 +272,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-type (quote ghci))
- '(package-selected-packages
+'(package-selected-packages
         (quote
          (writeroom-mode flycheck-haskell flycheck yasnippet ssh multiple-cursors emmet-mode company-ghc)))
  '(writeroom-mode-line t)
