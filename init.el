c057085cb777e2a07a0ab62f87523b1e85f19521
leejaeyong7 <leejaeyong7@gmail.com>
2016-02-13 15:13:43 -0600

    added snippets

--- init.el
+++ init.el
@@ -143,11 +143,11 @@
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 (add-hook 'neotree-mode-hook
-          (lambda ()
-            (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-            (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
-            (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-            (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+    (lambda ()
+        (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+        (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+        (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+        (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
 (setq projectile-switch-project-action 'neotree-projectile-action)
     
 ;;------------------------------------------------------------------------------
