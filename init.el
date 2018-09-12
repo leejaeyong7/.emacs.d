6f5fd62ce284e9982a32890c79faaabf1f1fe265
Jae Yong Lee <leejaeyong7@gmail.com>
2018-09-12 10:52:27 -0500

    removing color

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray17" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Inconsolata"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
  '(cursor ((t (:background "White"))))
  '(escape-glyph ((t (:foreground "green1"))))
  '(font-lock-comment-face ((t (:foreground "orange1"))))
